6bec7af1e1822f3855135e1da5c082dc3d0058bb
Nibesh Khadka <nibeshkhadka@gmail.com>
2019-01-10 11:04:52 +0200

    User Schema Initial Design

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+//Create UserSchema 
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
