3caaf138529793a84b26211f3a4e9dd66dce0817
Nibesh Khadka <nibeshkhadka@gmail.com>
2019-02-13 09:17:39 +0200

    Create Profile if not exist, during login

--- v2/routes/clientRoute/clientRotue.js
+++ v2/routes/clientRoute/clientRotue.js
@@ -1,7 +1,9 @@
 const express = require("express"),
   router = express.Router(),
-  profile = require("../../models/profile"),
+  clientProfile = require("../../models/clientProfile"),
   authentication = require("../../middlewares/authentication");
+
+const Events = require("../../models/trainingProgram");
 
 //This proivdes info about local user or current user
 router.use((req, res, next) => {
@@ -11,6 +13,30 @@ router.use((req, res, next) => {
 
 router.get("/", authentication.clientLoggedIn, (req, res) => {
   const page = { title: "Profile" };
+  clientProfile
+    .findOne({ user: req.user.id })
+    .then(profile => {
+      if (profile) {
+        console.log(profile);
+      } else {
+        const newProfile = {};
+        (newProfile.phone = req.user.phone),
+          (newProfile.user = req.user.id),
+          (newProfile.registered_date = req.user.registeredDate);
+        clientProfile
+          .create(newProfile)
+          .then(newProfile => {
+            console.log(newProfile);
+          })
+          .catch(err => {
+            throw err;
+          });
+      }
+    })
+    .catch(err => {
+      throw err;
+    });
+  console.log(req.user.id);
   res.render("client/profile", { page: page, currentUser: req.user });
 });
 
@@ -24,7 +50,24 @@ router.get("/profile", authentication.clientLoggedIn, (req, res) => {
 
 router.get("/events", authentication.clientLoggedIn, (req, res) => {
   const page = { title: "Events" };
-  res.render("client/events", { page: page, currentUser: req.user });
+  Events.find({})
+    .then(events => {
+      if (events) {
+        return res.render("client/events", {
+          page: page,
+          currentUser: req.user,
+          events: events
+        });
+      }
+    })
+    .catch(err => {
+      throw err;
+    });
+});
+
+//Join Events
+router.post("/events/join/:id", (req, res) => {
+  res.send("You have joined this event");
 });
 
 module.exports = router;
