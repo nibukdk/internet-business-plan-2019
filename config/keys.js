4f622a9e77f4ce9145fc64c79e3ee894ada095ed
Nibesh Khadka <nibeshkhadka@gmail.com>
2019-03-14 09:26:48 +0200

    Key change

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://admin:admin123@ds113866.mlab.com:13866/gymoid-app',
-    // mongoURI:'mongodb://localhost/gym-app',
+    //mongoURI: 'mongodb://admin:admin123@ds235775.mlab.com:35775/connector',
+     mongoURI:'mongodb://localhost/gym-app',
      secretOrKey:'secret'
  }
